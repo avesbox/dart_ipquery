b007ca25bfc2d7831d2dca28b0d97c052145416c
Francesco Vallone <vallonefrancesco587@gmail.com>
2025-03-20 00:18:46 +0100

    chore: add example

---
+++ example/lib/example.dart
@@ -0,0 +1,13 @@
+import 'package:dart_ipquery/dart_ipquery.dart';
+
+Future<void> main(List<String> arguments) async {
+  final client = IpQueryClient();
+  final res = await client.queryIp("8.8.8.8");
+  print(res);
+  final myIp = await client.queryOwnIp();
+  print(myIp);
+  final myIpQuery = await client.queryIp(myIp);
+  print(myIpQuery);
+  final ips = await client.queryBulk(["8.8.8.8", "1.1.1.1"]);
+  print(ips);
+}
